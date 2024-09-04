3269502f6376bd4069dacca108f65d4f83719213
Temitope Akintayo <temitope.akintayo@iisysgroup.com>
2024-09-04 14:38:39 +0100

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
+  e2e: { 
      chromeWebSecurity:false,
-     
+     watchForFileChanges:false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
